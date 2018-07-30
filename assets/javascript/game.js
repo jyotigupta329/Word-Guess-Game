1029fe0526ac87989b8ff1945e1d64ebc51d8560
jyoti <jyoti.gupta329@gmail.com>
2018-07-29 22:25:49 -0500

    Bug fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,22 +2,29 @@ var computerChoices = ["apple", "boy", "cost", "dog", "electric", "facebook", "g
     "india", "joke", "kite", "love", "monkey", "nation", "oskar", "pegion", "question", "rose",
     "sun", "time", "unicorn", "visa", "work", "xenon", "youtube", "zebra"]
 
+
 var win = 0;
 var loss = 0;
 var totalGuess = 10;
+var computerGuess;
+var flag = 0;
+var flagCom = 0;
 var displayUserGuess = [];
 var displayBlankSpace = [];
-var flag = 0;
 var res = [];
+var correctChoicePositions = [];
+//var correctChoice = [];
 var displayBlankSpaceEl = document.getElementById("displayBlankId");
 var guessEl = document.getElementById("guessId");
 var displayGuessEl = document.getElementById("displayUserGuessId");
 var losses = document.getElementById("lossId");
 var wins = document.getElementById("winId");
-// Randomly chooses a choice from the options array. This is the Computer's guess.
+
 
 init();
+
 document.onkeyup = function (event) {
+
 
     var userGuess = event.key;
 
@@ -32,56 +39,59 @@ document.onkeyup = function (event) {
         }
     }
 
+
     if (flag === 0) {
         totalGuess--;
+        guessEl.textContent = totalGuess;
         //Display the user guess
         displayUserGuess.push(userGuess);
-
         displayGuessEl.textContent = displayUserGuess.join(', ');
-
         //If user choice letter matches the computer choice it replace the blank space with the letter
         for (var i = 0; i < res.length; i++) {
             if (userGuess === res[i]) {
                 displayBlankSpace[i] = res[i];
-
-            } else {
-
+                correctChoicePositions.push(i);
+                displayBlankSpaceEl.textContent = displayBlankSpace.join(' ');
             }
-            displayBlankSpaceEl.textContent = displayBlankSpace.join(' ');
+
         }
 
+        var counter = comparision();
+        if (counter === 1 && totalGuess >= 0) {
+            win++;
+            wins.textContent = win;
+            alert("Congratulations!! You Won!! The word is : " + computerGuess);
+            init();
 
-        for (var i = 0; i < res.length; i++) {
-            console.log(res[i] !== displayBlankSpace[i] || totalGuess === 0);
-            if (res[i] !== displayBlankSpace[i] && totalGuess === 0) {
-
-                loss++;
-                losses.textContent = loss;
-                init();
-
-                //alert("Game Lost!");
-                break;
-            }
-            if (res[res.length - 1] === displayBlankSpace[displayBlankSpace.length - 1]) {
-                win++;
-                wins.textContent = win;
-                losses.textContent = loss;
-                init();
-                break;
-            }
-            break;
+            return;
         }
+        if (totalGuess < 0) {
+            loss++;
+            losses.textContent = loss;
+            alert("Sorry!! Try again!!");
+            init();
+            return;
 
-        guessEl.textContent = totalGuess;
+        }
     }
 
-};
+
+
+}
+
+function comparision() {
+    if (correctChoicePositions.length === res.length) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
 
 function init() {
     totalGuess = 10;
     displayUserGuess = [];
     displayBlankSpace = [];
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
     // Convert string to an array and Display the blank spaces
 
@@ -90,9 +100,7 @@ function init() {
     for (var i = 0; i < res.length; i++) {
         displayBlankSpace.push("_");
     }
-
     displayBlankSpaceEl.textContent = displayBlankSpace.join(' ');
     displayGuessEl.textContent = '';
     guessEl.textContent = 10;
 }
-
